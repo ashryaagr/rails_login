8bd61d9eb6c03e14a68bd71b40bfe4fcb3378f20
ashryaagr <ashryaagr@gmail.com>
2019-01-16 21:25:06 +0530

    heroku deployment

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_save {self.email = self.email.downcase}
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+    # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+end
